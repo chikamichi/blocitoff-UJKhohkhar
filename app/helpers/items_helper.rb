6ff15d120cf68b2a51e5de6cdf5c6224512db53f
Jean-Denis Vauguet <jd@vauguet.fr>
2015-03-21 09:14:52 +0100

    Display actual left time, with adaptive units
    Signed-off-by: Jean-Denis Vauguet <jd@vauguet.fr>

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,7 @@
 module ItemsHelper
+  # How much time is left before the specified item is expired?
+  def time_left(item)
+    return "No time left" if item.expired?
+    distance_of_time_in_words(item.validity.ago, item.created_at) + " left"
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,15 @@
 class Item < ActiveRecord::Base
   belongs_to :user
 
-  def days_left
-    (DateTime.now.to_date - created_at.to_date).to_i
+  VALIDITY_DURATION = 7.days
+
+  # Is the item expired? (ie. no time left).
+  def expired?
+    (created_at - validity).to_i < 0
+  end
+
+  # How long the item is considered valid once created.
+  def validity
+    self.class::VALIDITY_DURATION
   end
 end
