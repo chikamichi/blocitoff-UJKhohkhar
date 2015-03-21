19d7574f1e838b15ec1393227a207d1d52e37fab
Jean-Denis Vauguet <jd@vauguet.fr>
2015-03-21 07:10:25 +0100

    [fix] simpler html/js handling in ItemsController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,15 @@
 class ItemsController < ApplicationController
+  respond_to :html, :js
+
   def create
+    @user = User.find(params[:user_id])
     @item = current_user.items.create(item_params)
-    
-    respond_to do |format|
-      format.html
-      format.js
-    end
   end
 
   def destroy
     @user = User.find(params[:user_id])
     @item = Item.find(params[:id])
     @item.destroy
-    
-    respond_to do |format|
-      format.html
-      format.js
-    end
   end
 
   private
